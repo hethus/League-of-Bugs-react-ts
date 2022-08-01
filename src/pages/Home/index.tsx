b9a9f24694711a91922fcc48a3281766fac100ae
Giovanne Berteli Comba <joebcomba@gmail.com>
2022-08-01 16:39:26 -0300

    feature: added the assets

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,68 @@
+const Home = () => {
+  return (
+    <div>
+      <div>
+        <img alt="logo" />
+        <img alt="home" />
+        <img alt="settings" />
+        <img alt="logout" />
+      </div>
+      <div>
+        <header>
+          <div>
+            <h1>League of Bugs</h1>
+            <p>{new Date(Date.now()).toLocaleDateString()}</p>
+          </div>
+          <div>
+            <img src="search" alt="" />
+            <input />
+          </div>
+        </header>
+        <section>
+          <div>
+            <p>Bug points</p>
+          </div>
+          <div>
+            <h2>Choose the Bug point</h2>
+            <select name="" id="">
+              <option value="1">1</option>
+            </select>
+          </div>
+          <div>
+            <div>Card</div>
+          </div>
+        </section>
+      </div>
+      <aside>
+        <header>
+          <h2>Shopping cart</h2>
+        </header>
+        <div>
+          <div>
+            <h3>Item</h3>
+            <h3>Qty</h3>
+            <h3>Price</h3>
+          </div>
+          <div>
+            <div>Card checkout</div>
+            <div>Card checkout</div>
+            <div>Card checkout</div>
+          </div>
+        </div>
+        <div>
+          <div>
+            <p>Discount</p>
+            <p>R$0.00</p>
+          </div>
+          <div>
+            <p>Subtotal</p>
+            <p>R$0.00</p>
+          </div>
+          <button>Continue to payment</button>
+        </div>
+      </aside>
+    </div>
+  );
+}
+
+export default Home;
