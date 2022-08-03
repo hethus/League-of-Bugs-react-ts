74c945ef1153f5b7feb033f724b7007a6d2c5193
Giovanne Berteli Comba <joebcomba@gmail.com>
2022-08-03 16:24:42 -0300

    feature: created the colors and the constants files

---
+++ src/assets/styles/colors.ts
@@ -0,0 +1,17 @@
+export const colors = {
+  primaryColor: "#6cea69",
+  secondaryColor: "#e26b6b",
+
+  primaryColorOpacity: "rgba(108, 234, 105, 0.4)",
+
+  baseBg1: "#252836",
+  baseBg2: "#1f1d2b",
+  baseForm: "#2d303e",
+  baseLine: "#393c49",
+
+  textColor: "#ffffff",
+  textLighter: "#e0e6e9",
+  textLight: "#abbbc2",
+  textNeutral: "#889898",
+  textDark: "#3b5162",
+}
